22bb502f95a73c8b1e52e166a21bfca7772e1031
Justin Wu <71717267+FlameBun@users.noreply.github.com>
2025-02-21 23:54:08 -0500

    Add floating point operation count

--- Problem1.java
+++ Problem1.java
@@ -4,13 +4,13 @@ public class Problem1 {
 
 	// f(x), f'(x), and slope formula
 	public static double f(double x) {
-		return Math.exp(-Math.pow(x, 3)) - Math.pow(x, 4) - Math.sin(x);
+		return Math.exp(-Math.pow(x, 3)) - Math.pow(x, 4) - Math.sin(x); // 80 floating point operations
 	}
 	public static double fPrime(double x) {
-		return -3 * Math.pow(x, 2) * Math.exp(-Math.pow(x, 3)) - 4 * Math.pow(x, 3) - Math.cos(x);
+		return -3 * Math.pow(x, 2) * Math.exp(-Math.pow(x, 3)) - 4 * Math.pow(x, 3) - Math.cos(x); // 84 floating point operations
 	}
 	public static double slope(double x1, double x2) {
-		return (f(x2) - f(x1)) / (x2 - x1);
+		return (f(x2) - f(x1)) / (x2 - x1); // 163 floating point operations
 	}
 	
 	// Method 1: Bisection method
@@ -28,7 +28,7 @@ public class Problem1 {
 			
 			estimatedRoot = (a + b) / 2;
 			i++;
-			numOps += 4;
+			numOps += 164;
 		}
 		
 		System.out.println("Method 1 (Bisection Method):");
@@ -42,14 +42,17 @@ public class Problem1 {
 		double x = 0; // Estimated root
 		
 		int i = 0; // # of iterations
+		int numOps = 0; // # of floating point operations
 		while (Math.abs(x - actualRoot) >= 0.00005) {
 			x = x - f(x) / fPrime(x);
 			i++;
+			numOps += 163;
 		}
 		
 		System.out.println("Method 2 (Newton Method):");
 		System.out.println("Estimated Root: " + x);
 		System.out.println("# of Iterations: " + i);
+		System.out.println("# of Floating Point Operations: " + numOps);
 	}
 	
 	// Method 3: Secant method
@@ -58,17 +61,20 @@ public class Problem1 {
 		double secondRoot = 1; // Estimated root
 		
 		int i = 0; // # of iterations
+		int numOps = 0; // # of floating point operations
 		while (Math.abs(secondRoot - actualRoot) >= 0.00005) {
 			double thirdRoot = secondRoot - f(secondRoot) / slope(firstRoot, secondRoot);
 			firstRoot = secondRoot;
 			secondRoot = thirdRoot;
 			
 			i++;
+			numOps += 246;
 		}
 		
 		System.out.println("Method 3 (Secant Method):");
 		System.out.println("Estimated Root: " + secondRoot);
 		System.out.println("# of Iterations: " + i);
+		System.out.println("# of Floating Point Operations: " + numOps);
 	}
 	
 	// Method 4: Monte Carlo method
@@ -76,15 +82,18 @@ public class Problem1 {
 		double randomRoot = 0.5 + Math.random() * 0.25; // Random floating point value on [0.5, 0.75]
 		
 		int i = 0; // # of iterations
+		int numOps = 3; // # of floating point operations
 		while (Math.abs(randomRoot - actualRoot) >= 0.00005) {
 			// Generate random floating point value on the interval [0.5, 0.75]
 			randomRoot = 0.5 + Math.random() * 0.25;
 			i++;
+			numOps += 4;
 		}
 		
 		System.out.println("Method 4 (Monte Carlo Method):");
 		System.out.println("Estimated Root: " + randomRoot);
 		System.out.println("# of Iterations: " + i);
+		System.out.println("# of Floating Point Operations: " + numOps);
 	}
 	
 	public static void main(String[] args) {
